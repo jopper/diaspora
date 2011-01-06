623a103d748997f5390811e8eab98800d436ff40
Philip Champon <pchampon@gmail.com>
2011-01-05 21:24:43 -0500

    Task 113  add simple reminder to update, and show
    Final commits, allowing us to display how fresh our fork is

--- app/helpers/modified_helper.rb
+++ app/helpers/modified_helper.rb
@@ -1,11 +1,33 @@
-require 'ftools'
+#require 'ftools'
 require 'time'
 
 module ModifiedHelper
   def last_modified
-    #filepath = Rails.root.join('tmp', '.last_pull');
-    #if (File.mtime(filepath) > Time.now-(60*5))
-    #end
-    `git log -1 --pretty=format:"%ar"`
+    git_last='git log -1 --pretty=format:"%ar"'
+    filepath = Rails.root.join('tmp', '.last_pull')
+    begin
+      mtime = File.mtime(filepath)
+    rescue Exception => e
+      mtime = nil
+    end
+    if (mtime.nil? || mtime < Time.now-(60*5))
+      last=`#{git_last}`
+      begin
+        f = File.open(filepath, 'w')
+        f.puts(last)
+        f.close
+      rescue  Exception => e
+        Rails.log.info("Failed to log git status #{filepath}: #{e}")
+      end
+    else
+      begin
+        arr=IO.readlines(filepath)
+        last=arr[0]
+      rescue Exception => e
+        Rails.log.info("Failed to read git status #{filepath}: #{e}")
+        last=`#{git_last}`
+      end
+    end
+    return last
   end
 end
