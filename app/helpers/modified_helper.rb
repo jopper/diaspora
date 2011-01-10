10b9e864825ecd07276d926fae0408db6b415442
Philip Champon <pchampon@gmail.com>
2011-01-10 14:57:59 -0500

    added smarter control for caching

--- app/helpers/modified_helper.rb
+++ app/helpers/modified_helper.rb
@@ -5,29 +5,36 @@ module ModifiedHelper
   def last_modified
     git_last='git log -1 --pretty=format:"%ar"'
     filepath = Rails.root.join('tmp', '.last_pull')
-    begin
-      mtime = File.mtime(filepath)
-    rescue Exception => e
-      mtime = nil
+    time_min = 60
+
+    if File.writable?(filepath)
+      begin
+        mtime = File.mtime(filepath)
+        last = IO.readlines(filepath).at(0)
+      rescue Exception => e
+        Rails.logger.info("Failed to read git status #{filepath}: #{e}")
+      end
+
+      if last =~ /minutes/
+        time_min = 60*5
+      elsif last =~ /hours/
+        time_min = 60*60
+      else
+        time_min = 60*60*24
+      end
     end
-    if (mtime.nil? || mtime < Time.now-(60*5))
-      last=`#{git_last}`
+
+    if (mtime.nil? || mtime < Time.now-time_min)
+      last = `#{git_last}`
       begin
         f = File.open(filepath, 'w')
         f.puts(last)
         f.close
       rescue  Exception => e
-        Rails.log.info("Failed to log git status #{filepath}: #{e}")
-      end
-    else
-      begin
-        arr=IO.readlines(filepath)
-        last=arr[0]
-      rescue Exception => e
-        Rails.log.info("Failed to read git status #{filepath}: #{e}")
-        last=`#{git_last}`
+        Rails.logger.info("Failed to log git status #{filepath}: #{e}")
       end
     end
-    return last
+
+    last
   end
 end
